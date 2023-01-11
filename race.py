1a0ea6531c682b47c15f16225a97ca0247ecd02e
hugo-epsi <121803919+hugo-epsi@users.noreply.github.com>
2023-01-11 16:19:51 +0100

    complete first version

--- race.py
+++ race.py
@@ -1,7 +1,38 @@
-class Vaisseau():
-    def __init__(self):
-        pass
+class SpaceShip():
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed  
 
-class Circuit():
-    def __init__(self):
-        pass
+    def get_name(self):
+        return self.name
+
+    def get_speed(self):
+        return self.speed
+
+
+class Track():
+    def __init__(self, length):
+        self.length = length
+        self.racers = []
+
+    def AddSpaceShip(self, racer):
+        self.racer = racer
+        self.racers.append(racer)
+
+    def StartRace(self, racers):
+        print(self.racers)
+        fastest = 0
+        for i in racers:
+            if racers[i].get_speed() > fastest:
+                fastest = racers[i].get_name()
+        return fastest
+
+
+track1 = Track(2000)
+spaceship1 = SpaceShip("space1", 5)
+spaceship2 = SpaceShip("space2", 6)
+spaceship3 = SpaceShip("space3", 3)
+spaceship4 = SpaceShip("space4", 8)
+spaceship5 = SpaceShip("space5", 2)
+
+track1.AddSpaceShip(spaceship2)
